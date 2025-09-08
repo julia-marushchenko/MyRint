49625f1657b4b10cfb1e5356ec2fe8d21ad3f2b0
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-09-08 03:11:19 +0200

    Add files via upload
    Committing Main class

---
+++ src/com/math/Main.java
@@ -0,0 +1,18 @@
+// Java program to use Math.rint(double/float num) method
+
+package com.math;
+
+// Main class
+public class Main {
+
+    // Main method to run java program
+    public static void main(String[] args) {
+
+        // Printing rounding to nearest int examples
+       System.out.println( Math.rint(2.6)); // Output: 3.0
+        System.out.println( Math.rint(2.5)); // Output: 2.0
+        System.out.println( Math.rint(2.4)); // Output: 2.0
+        System.out.println( Math.rint(2.7)); // Output: 3.0
+        System.out.println( Math.rint(2.7f)); // Output: 3.0
+    }
+}
